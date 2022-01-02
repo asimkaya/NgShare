c0d6bca2e88f08e8c792f496fa9043b76a91ad7b
Asım Emre Kaya <58777354+asimkaya@users.noreply.github.com>
2022-01-02 20:21:38 +0300

    routing and login

--- ngShare_frontend/src/app/app.module.ts
+++ ngShare_frontend/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocialLoginModule, SocialAuthServiceConfig, GoogleLoginProvider  } from 'angularx-social-login';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AuthGuardService } from './auth-guard.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '398635372316-hq2sjc9e4bb9r8g64djp27erps9ddn0k.apps.googleusercontent.com'
+            )
+          }
+        ]
+      } 
+    },
+   AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
