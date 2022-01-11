68a6e14b9d1b50967112e8e1d0816a5a1d901148
Asım Emre Kaya <58777354+asimkaya@users.noreply.github.com>
2022-01-11 23:12:34 +0300

    npx create-react-app

---
+++ ngShare_frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className='text-3x1 font-bold underline'>
+            Hello World
+        </div>
+    )
+}
+
+export default App
