f38ded92a8df75d3f9004a4c1caedcef30f8f996
Asım Emre Kaya <58777354+asimkaya@users.noreply.github.com>
2022-01-12 22:46:14 +0300

    prject stc

--- ngShare_frontend/src/App.js
+++ ngShare_frontend/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {Routes,Route, useNavigate} from 'react-router-dom';
 
 const App = () => {
     return (
-        <div className='text-3x1 font-bold underline'>
-            Hello World
-        </div>
+        <Routes>
+            <Route path="login" element={<Login/>}/>
+            <Route path="/*" element={<Home/>}/>
+        </Routes>
     )
 }
 
